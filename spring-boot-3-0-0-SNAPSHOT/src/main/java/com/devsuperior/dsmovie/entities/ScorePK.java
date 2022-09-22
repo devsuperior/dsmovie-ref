3e5a16f2f8dc7f49002d2938bafe83d1156898db
Alexandre <alexandre.si.ufu@gmail.com>
2022-09-22 17:26:24 -0300

    security conf, domain model - spring-boot-3-0-0-SNAPSHOT

---
+++ spring-boot-3-0-0-SNAPSHOT/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dsmovie.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class ScorePK implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public ScorePK() {
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
---
+++ spring-boot-3-0-0-SNAPSHOT/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dsmovie.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	
+	public User() {
+	}
+
+	public User(Long id, String email) {
+		this.id = id;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
